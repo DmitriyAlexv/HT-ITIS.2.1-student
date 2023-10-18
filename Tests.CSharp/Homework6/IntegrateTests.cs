4f13a85126a2c9bf3da14c154fecac1601d0e3e8
Dmitriy Aleksandrov <dmitriy_aleksandrov_2004@bk.ru>
2023-10-18 19:43:38 +0300

    Refactor code

--- Tests.CSharp/Homework6/IntegrateTests.cs
+++ Tests.CSharp/Homework6/IntegrateTests.cs
@@ -17,13 +17,7 @@ public class IntegrateTests: IClassFixture<CustomWebApplicationFactory<App.Start
     [Homework(Homeworks.HomeWork6)]
     public async Task TestRootRouteReturnsExpectedResponse()
     {
-        // Act
-        var response = await _client.GetAsync("/");
-        var responseString = await response.Content.ReadAsStringAsync();
-        
-        // Assert
-        Assert.True(response.IsSuccessStatusCode);
-        Assert.Equal("Use //calculate?value1=<VAL1>&operation=<OPERATION>&value2=<VAL2>", responseString);
+        await RunTest("/", "Use //calculate?value1=<VAL1>&operation=<OPERATION>&value2=<VAL2>", HttpStatusCode.OK);
     }
     
     [HomeworkTheory(Homeworks.HomeWork6)]
@@ -33,55 +27,38 @@ public class IntegrateTests: IClassFixture<CustomWebApplicationFactory<App.Start
     [InlineData("/calculate?value1=2&operation=Plus")]
     public async Task TestCalculateRouteWithoutOneMoreParamReturnsExpectedError(string requestUri)
     {
-        // Arrange
-        var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
-
-        // Act
-        var response = await _client.SendAsync(request);
-        var responseString = await response.Content.ReadAsStringAsync();
-        
-        // Assert
-        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
-        Assert.Contains("Missing value for required property", responseString);
+        await RunTest(requestUri, "Missing value for required property", HttpStatusCode.BadRequest);
     }
 
     [Homework(Homeworks.HomeWork6)]
     public async Task TestCalculateRouteReturnsExpectedResult()
     {
-        // Arrange
-        var request = new HttpRequestMessage(HttpMethod.Get, "/calculate?value1=2&operation=Plus&value2=3");
-
-        // Act
-        var response = await _client.SendAsync(request);
-        var responseString = await response.Content.ReadAsStringAsync();
-        
-        // Assert
-        Assert.True(response.IsSuccessStatusCode);
-        Assert.Equal("5", responseString);
+        await RunTest("/calculate?value1=2&operation=Plus&value2=3", "5", HttpStatusCode.OK);
     }
 
     [Homework(Homeworks.HomeWork6)]
     public async Task TestInvalidRequestReturnsExpectedError()
     {
-        // Arrange
-        var request = new HttpRequestMessage(HttpMethod.Get,"/calculate?value1=NotParsable&operation=NotParsable&value2=NotParsable");
-        
-        // Act
-        var response = await _client.SendAsync(request);
-        var responseString = await response.Content.ReadAsStringAsync();
-        
-        // Assert
-        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
-        Assert.Contains("Could not parse", responseString);
+        await RunTest("/calculate?value1=NotParsable&operation=NotParsable&value2=NotParsable", "Could not parse", HttpStatusCode.BadRequest);
     }
 
     [Homework(Homeworks.HomeWork6)]
     public async Task TestNotExistedRouteReturnsExpectedError()
     {
+        await RunTest("/NotExist", "Not Found", HttpStatusCode.NotFound);
+    }
+
+    private async Task RunTest(string query, string expectedValue, HttpStatusCode expectedStatusCode)
+    {
+        // Arrange
+        var request = new HttpRequestMessage(HttpMethod.Get, query);
+
         // Act
-        var response = await _client.GetAsync("/NotExist");
+        var response = await _client.SendAsync(request);
+        var responseString = await response.Content.ReadAsStringAsync();
 
         // Assert
-        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        Assert.Equal(expectedStatusCode, response.StatusCode);
+        Assert.Contains(expectedValue, responseString);
     }
 }
