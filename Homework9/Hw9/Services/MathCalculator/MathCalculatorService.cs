0e4f48e0dea747a514a2102601065a64a5f69615
Dmitriy Aleksandrov <dmitriy_aleksandrov_2004@bk.ru>
2023-11-15 12:25:17 +0300

    Did HomeWork_9

--- Homework9/Hw9/Services/MathCalculator/MathCalculatorService.cs
+++ Homework9/Hw9/Services/MathCalculator/MathCalculatorService.cs
@@ -1,11 +1,22 @@
+using System.Linq.Expressions;
 using Hw9.Dto;
+using Hw9.ErrorMessages;
 
 namespace Hw9.Services.MathCalculator;
 
 public class MathCalculatorService : IMathCalculatorService
 {
+    private readonly ExpressionBuilder.ExpressionBuilder _expressionBuilder = new();
     public async Task<CalculationMathExpressionResultDto> CalculateMathExpressionAsync(string? expression)
     {
-        throw new NotImplementedException();
+        var buildResult = await _expressionBuilder.CreateExpressionFromString(expression);
+        if (!buildResult.IsSuccess)
+            return new CalculationMathExpressionResultDto(buildResult.ErrorMessage!);
+
+        var mathResult = ((Func<double>)Expression.Lambda(buildResult.MathExpression!).Compile()).Invoke();
+        
+        return  Double.IsInfinity(mathResult) ? 
+            new CalculationMathExpressionResultDto(MathErrorMessager.DivisionByZero) :
+            new CalculationMathExpressionResultDto(mathResult);
     }
 }
