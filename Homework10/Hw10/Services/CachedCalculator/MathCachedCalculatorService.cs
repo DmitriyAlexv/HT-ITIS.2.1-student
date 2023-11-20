5e64fedbd38df41c4607a92e9366b0c582ba4237
Dmitriy Aleksandrov <dmitriy_aleksandrov_2004@bk.ru>
2023-11-20 12:31:46 +0300

    Did Homework_10

--- Homework10/Hw10/Services/CachedCalculator/MathCachedCalculatorService.cs
+++ Homework10/Hw10/Services/CachedCalculator/MathCachedCalculatorService.cs
@@ -1,6 +1,7 @@
 ﻿using Hw10.DbModels;
 using Hw10.Dto;
 using Hw10.Services.MathCalculator;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hw10.Services.CachedCalculator;
 
@@ -17,6 +18,20 @@ public class MathCachedCalculatorService : IMathCalculatorService
 
 	public async Task<CalculationMathExpressionResultDto> CalculateMathExpressionAsync(string? expression)
 	{
-		throw new NotImplementedException();
+		
+		if (await _dbContext.SolvingExpressions.Select(x => x.Expression).ContainsAsync(expression))
+		{
+			await Task.Delay(1000);
+			var cachedResult = await _dbContext.SolvingExpressions
+				.FirstAsync(solvingExpression => solvingExpression.Expression == expression);
+			return new CalculationMathExpressionResultDto(cachedResult.Result);
+		}
+		var result = await _simpleCalculator.CalculateMathExpressionAsync(expression);
+		if (!result.IsSuccess)
+			return result;
+		
+		await _dbContext.SolvingExpressions.AddAsync(new SolvingExpression() { Expression = expression!, Result = result.Result });
+		await _dbContext.SaveChangesAsync();
+		return result;
 	}
 }
--- Homework10/Hw10/Services/MathCalculator/MathCalculatorService.cs
+++ Homework10/Hw10/Services/MathCalculator/MathCalculatorService.cs
@@ -1,11 +1,57 @@
+using System.Diagnostics.CodeAnalysis;
 using Hw10.Dto;
+using Hw10.Services.GraphBuilder;
+using Hw10.ErrorMessages;
+using System.Linq.Expressions;
 
 namespace Hw10.Services.MathCalculator;
 
 public class MathCalculatorService : IMathCalculatorService
 {
+    private readonly ExpressionBuilder.ExpressionBuilder _expressionBuilder = new();
+    private readonly GraphBuilder.GraphBuilder _graphBuilder = new();
+
     public async Task<CalculationMathExpressionResultDto> CalculateMathExpressionAsync(string? expression)
     {
-        throw new NotImplementedException();
+        var buildResult = await _expressionBuilder.CreateExpressionFromString(expression);
+        if (!buildResult.IsSuccess)
+            return new CalculationMathExpressionResultDto(buildResult.ErrorMessage!);
+
+        var graph = _graphBuilder.BuildGraph(buildResult.MathExpression!);
+
+        var result = await CalculateAsync(buildResult.MathExpression!, graph);
+
+        return Double.IsInfinity(result)
+            ? new CalculationMathExpressionResultDto(MathErrorMessager.DivisionByZero)
+            : new CalculationMathExpressionResultDto(result);
     }
+
+    private static async Task<double> CalculateAsync(Expression current,
+        IReadOnlyDictionary<Expression, MathExpression> dependencies)
+    {
+        if (!dependencies.ContainsKey(current))
+        {
+            return double.Parse(current.ToString());
+        }
+
+        await Task.Delay(1000);
+        var left = Task.Run(() =>
+            CalculateAsync(dependencies[current].LeftExpression, dependencies));
+        var right = Task.Run(() =>
+            CalculateAsync(dependencies[current].RightExpression, dependencies));
+
+        var results = await Task.WhenAll(left, right);
+        return CalculateExpression(results[0], current.NodeType, results[1]);
+    }
+
+    [ExcludeFromCodeCoverage]
+    private static double CalculateExpression(double value1, ExpressionType expressionType, double value2) =>
+        expressionType switch
+        {
+            ExpressionType.Add => value1 + value2,
+            ExpressionType.Subtract => value1 - value2,
+            ExpressionType.Divide => value2 == 0 ? double.PositiveInfinity : value1 / value2,
+            ExpressionType.Multiply => value1 * value2,
+            _ => 0
+        };
 }
