8325d4d7d5dcc1250a29b6cc46fec8cba6de6f17
Dmitriy Aleksandrov <dmitriy_aleksandrov_2004@bk.ru>
2023-12-12 11:38:47 +0300

    Did Homework_13

---
+++ Homework13/Cached/Configuration/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+using Hw10.Services;
+using Cached.Services.CachedCalculator;
+using Hw10.Services.MathCalculator;
+
+namespace Cached.Configuration;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddMathCalculator(this IServiceCollection services)
+    {
+        return services.AddTransient<MathCalculatorService>();
+    }
+    
+    public static IServiceCollection AddCachedMathCalculator(this IServiceCollection services)
+    {
+        return services.AddScoped<IMathCalculatorService>(s =>
+            new MathCachedCalculatorService(
+                s.GetRequiredService<MathCalculatorService>()));
+    }
+}
---
+++ Homework13/Cached/Services/CachedCalculator/MathCachedCalculatorService.cs
@@ -0,0 +1,32 @@
+﻿using Hw10.Dto;
+using Hw10.Services;
+
+namespace Cached.Services.CachedCalculator;
+
+public class MathCachedCalculatorService : IMathCalculatorService
+{
+	private readonly Dictionary<string, double> _cachedResults = new();
+	private readonly IMathCalculatorService _simpleCalculator;
+
+	public MathCachedCalculatorService(IMathCalculatorService simpleCalculator)
+	{
+		_simpleCalculator = simpleCalculator;
+	}
+
+	public async Task<CalculationMathExpressionResultDto> CalculateMathExpressionAsync(string? expression)
+	{
+		
+		if (_cachedResults.ContainsKey(expression!))
+		{
+			await Task.Delay(1000);
+			var cachedResult = _cachedResults[expression!];
+			return new CalculationMathExpressionResultDto(cachedResult);
+		}
+		var result = await _simpleCalculator.CalculateMathExpressionAsync(expression);
+		if (!result.IsSuccess)
+			return result;
+		
+		_cachedResults.Add(expression!, result.Result);
+		return result;
+	}
+}
